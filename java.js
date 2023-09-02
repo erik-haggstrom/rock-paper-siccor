38f4f57fdbb7003474d14263c0139ec0704cc13f
erik-haggstrom <erik.haggstrom0@gmail.com>
2023-09-02 19:48:14 +0200

    Done with the UI

--- java.js
+++ java.js
@@ -1,5 +1,82 @@
-const box = document.querySelector('.box')
-const knapp = document.querySelector('.knapp')
+const knappRock = document.querySelector('.rock');
+const knappPaper = document.querySelector('.paper');
+const knappScissors = document.querySelector('.scissors');
+const resultat = document.querySelector('.resultat');
+const spelare = document.querySelector('.spelare')
+const dator = document.querySelector('.dator')
+
+let scores = {playerWins: 0, computerWins: 0};
+
+function rock() {
+  let playerSelection = "rock";
+  let computerSelection = getComputerChoice();
+  playRound(playerSelection, computerSelection, scores);
+  dator.textContent = scores.computerWins;
+  spelare.textContent = scores.playerWins;
+
+  if (scores.playerWins == 5){
+    resultat.textContent = 'You won!'
+    knappRock.removeEventListener('click', rock);
+    knappPaper.removeEventListener('click', paper);
+    knappScissors.removeEventListener('click', scissors);
+  }
+  else if (scores.computerWins == 5){
+    resultat.textContent = 'The computer won'
+    knappRock.removeEventListener('click', rock);
+    knappPaper.removeEventListener('click', paper);
+    knappScissors.removeEventListener('click', scissors);
+  }
+}
+
+function paper() {
+  let playerSelection = "paper";
+  let computerSelection = getComputerChoice();
+  playRound(playerSelection, computerSelection, scores);
+  spelare.textContent = scores.playerWins;
+  dator.textContent = scores.computerWins;
+
+  if (scores.playerWins == 5){
+    resultat.textContent = 'You won!'
+    knappRock.removeEventListener('click', rock);
+    knappPaper.removeEventListener('click', paper);
+    knappScissors.removeEventListener('click', scissors);
+  }
+  else if (scores.computerWins == 5){
+    resultat.textContent = 'The computer won'
+    knappRock.removeEventListener('click', rock);
+    knappPaper.removeEventListener('click', paper);
+    knappScissors.removeEventListener('click', scissors);
+  }
+}
+
+function scissors() {
+  let playerSelection = "scissors";
+  let computerSelection = getComputerChoice();
+  playRound(playerSelection, computerSelection, scores);
+  dator.textContent = scores.computerWins;
+  spelare.textContent = scores.playerWins;
+
+  if (scores.playerWins == 5){
+    resultat.textContent = 'You won!'
+    knappRock.removeEventListener('click', rock);
+    knappPaper.removeEventListener('click', paper);
+    knappScissors.removeEventListener('click', scissors);
+  }
+  else if (scores.computerWins == 5){
+    resultat.textContent = 'The computer won'
+    knappRock.removeEventListener('click', rock);
+    knappPaper.removeEventListener('click', paper);
+    knappScissors.removeEventListener('click', scissors);
+  }
+}
+
+knappRock.addEventListener('click', rock)
+  
+
+knappPaper.addEventListener('click', paper)
+  
+
+knappScissors.addEventListener('click', scissors)
 
 
 function getComputerChoice() {
@@ -9,50 +86,22 @@ return computerChoice[Math.floor(Math.random() * 3)];
 
 function playRound (playerSelection, computerSelection, scores) {
 if (playerSelection === computerSelection){
-  console.log('It is a draw')
+  resultat.textContent = 'It is a draw'
 }
 else if(
   (playerSelection === "rock" && computerSelection === "scissors") ||
   (playerSelection === "paper" && computerSelection === "rock") ||
   (playerSelection === "scissors" && computerSelection === "paper")
 ){
-  console.log(`You win ${playerSelection} beats ${computerSelection} `);
+  resultat.textContent = `You win ${playerSelection} beats ${computerSelection} `;
   scores.playerWins++;
 }
 else{
-  console.log(`The computer wins ${computerSelection} beats ${playerSelection} `);
+  resultat.textContent = `The computer wins ${computerSelection} beats ${playerSelection} `;
   scores.computerWins++;
 }
-}
-
-function game() {
-let scores = {playerWins: 0, computerWins: 0};
-
-for (let i = 0; i < 1000; i++){
-  let playerSelection = prompt('Rock paper or scissors?: ').toLowerCase();
-  let computerSelection = getComputerChoice();
-  playRound (playerSelection, computerSelection, scores);
-  console.log(scores.playerWins)
-
-  if (scores.playerWins === 3){
-    console.log('You won the game!!');
-    break;
-  }
-  else if (scores.computerWins === 3){
-    console.log('The computer won the game!!');
-    break;
-  }
-}
-if (scores.playerWins !== 3 && scores.playerWins > scores.computerWins){
-  console.log('You got the most wins congrats!!')
-}
-else if (scores.computerWins !== 3 && scores.computerWins > scores.playerWins){
-  console.log('The computer got the most wins YOU LOSE!!')
-}
-else if (scores.playerWins === scores.computerWins){
-  console.log("You and the computer won the same amout of games!")
-}
 
 }
+
 
 
